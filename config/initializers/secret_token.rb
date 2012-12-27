5a93076d869e50a5806345eb70a346b5c94332e0
Michael Lai <themichaellai@gmail.com>
2012-12-27 17:45:59 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wocky::Application.config.secret_token = '0b1de49e6b63dcb4609516b6db8b352de8efe5755653faa55d128e9a421c24e89ce7bea132abbaab5e3375da79c6880075645ce5156950597707b7c05c42e5ca'
